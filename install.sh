5e482162f70eac5d5a38efa6b89db0ff207b62d6
g <g@air.local>
2015-01-01 18:36:31 +1100

    more vim and zshell dotfiles

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo "symlink: $DIR/.zshrc -> $HOME/.zshrc"
 ln -s $DIR/.zshrc $HOME/.zshrc
+
+echo "symlink: $DIR/.vimrc -> $HOME/.vimrc"
+ln -s $DIR/.vimrc $HOME/.vimrc
